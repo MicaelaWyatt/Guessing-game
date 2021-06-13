9ce999ea270d3adea4beade8675dad2b51b3b00f
Micaela Colpi <micaelacolpi@gmail.com>
2021-06-13 16:25:06 -0500

    Phase 2

--- guessingGame/Program.cs
+++ guessingGame/Program.cs
@@ -6,9 +6,19 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
+            int secretNumber = 42;
             Console.WriteLine("??Guess the secret number??");
             string usersGuess = Console.ReadLine();
-            Console.WriteLine(Convert.ToInt32(usersGuess));
+            int Guess = Convert.ToInt32(usersGuess);
+            // Console.WriteLine(Convert.ToInt32(usersGuess));
+            if (Guess == secretNumber)
+            {
+                Console.WriteLine("Dang! Did you cheat?");
+            }
+            else
+            {
+                Console.WriteLine("That was so off. Do you wanna try again?");
+            }
         }
     }
 }
